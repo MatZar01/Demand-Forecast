d0dbbeeba9f14b3853117ab8ceedd74742686153
MatZar01 <zski.mateusz@gmail.com>
2024-07-08 16:27:02 +0200

    data management

--- src/argvs.py
+++ src/argvs.py
@@ -2,16 +2,13 @@ import yaml
 import sys
 
 
-def get_args() -> dict:
-
-    default_name = 'default.yml'
-    default_path = './cfgs/'
-    default_path = '/home/mateusz/Desktop/Demand-Forecast/cfgs/default.yml'
+def get_args(path) -> dict:
+    default_path = path
 
     try:
         model_info = yaml.load(open(f'{default_path}', 'r'), Loader=yaml.Loader)
     except FileNotFoundError:
         model_info = None
-        print(f'[ERROR] Config "{default_name}" not found \n[INFO] Aborting')
+        print(f'[ERROR] Config "{default_path}" not found \n[INFO] Aborting')
         sys.exit()
     return model_info
--- src/datasets.py
+++ src/datasets.py
@@ -1,49 +1,56 @@
 import numpy as np
-import torch
+import pandas as pd
 
 class DataSet:
-    def __init__(self, paths, recombine_data: list = [0.6, 0.2, 0.2]):
+    def __init__(self, paths, recombine_data: list = [0.7, 0.3], shuffle: bool = True, seed: int = 24):
         self.path_train = paths['TRAIN']
         self.path_test = paths['TEST']
         self.name = self.path_train.split('/')[-2]
         self.recombine_data = recombine_data
+        self.shuffle = shuffle
+        self.seed = seed
 
         self.train = None
         self.val = None
         self.test = None
         self.header = None
+        self.header_test = None
 
         self.load_data()
 
     def load_data(self):
-        train = None
-        test = None
-
-        # try header
+        # try training data
         try:
-            self.header = open(self.path_train, 'r').readline()
+            df_train = pd.read_csv(self.path_train)
+            self.header = [*df_train]
         except FileNotFoundError:
-            self.header = None
+            df_train = None
 
-        # try traning csv
+        # try testing data
         try:
-            train = np.loadtxt(self.path_train, dtype=str, skiprows=1)
+            df_test = pd.read_csv(self.path_test)
+            self.test = df_test.to_numpy()
+            self.header_test = [*df_test]
         except FileNotFoundError:
-            train = None
-
-        # try testing csv
-        try:
-            test = np.loadtxt(self.path_test, dtype=str, skiprows=1)
-        except FileNotFoundError:
-            test = None
+            df_test = None
 
         if self.recombine_data:
-            self.recombine(train, test)
+            self.recombine(df_train)
 
+    def recombine(self, train):
+        data = train.to_numpy()
+        if self.shuffle:
+            indexes = self.get_shuffled_indexes(data)
+            data = data[indexes]
 
-    def recombine(self, train, test):
-        if test is None:
-            data = train
-        else:
-            data = np.hstack([train, test])
-        print(data.shape)
+        train_len = int(data.shape[0]*self.recombine_data[0])
+        self.train = data[:train_len, :]
+        self.val = data[train_len:, :]
+        print(f'[INFO] Data:\ntrain: {self.train.shape[0]} points\nval: {self.val.shape[0]}\ntest: {self.test.shape[0]}')
+        print(f'Header: {self.header}')
+
+    def get_shuffled_indexes(self, data):
+        data_idx = list(range(data.shape[0]))
+        np.random.seed(self.seed)
+        np.random.shuffle(data_idx)
+        return data_idx
