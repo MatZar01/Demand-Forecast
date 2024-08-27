16d705fda7c135bad0ffa9fc92102ab2aa01f127
MatZar02 <mateusz.zarski@newsecuritysystems.pl>
2024-08-27 12:03:04 +0200

    mlp model finetuning, MLP embedding, finetuning results

--- baseline/MLP_tests.py
+++ baseline/MLP_tests.py
@@ -1,6 +1,6 @@
-from base_src import MLP_dataset
+from base_src import MLP_dataset, MLP_dataset_emb
 from base_src import get_matches
-from base_src import MLP
+from base_src import MLP, MLP_emb
 from base_src import L_Net
 import torch
 from torch.utils.data import DataLoader
@@ -32,14 +32,14 @@ embedders = {'C2': {'onehot': '/home/mateusz/Desktop/Demand-Forecast/embedding_m
              'C3': {'onehot': '/home/mateusz/Desktop/Demand-Forecast/embedding_models/onehot_C3.pkl',
                     'cat2vec': '/home/mateusz/Desktop/Demand-Forecast/embedding_models/embedder_c3.pth'}}
 
-model = torch.load('/home/mateusz/Desktop/Demand-Forecast/baseline/results_mlp/mlp_model.pth')
+model = torch.load('/home/mateusz/Desktop/Demand-Forecast/baseline/results_mlp/embedding/mlp_model.pth')
 loss_fn = RMSELoss()
 
 if not EMBED:
     embedders = None
 
 
-OUT_PATH = '/home/mateusz/Desktop/Demand-Forecast/baseline/results_mlp/whole_model_test.pkl'
+OUT_PATH = '/home/mateusz/Desktop/Demand-Forecast/baseline/results_mlp/embedding/whole_model_test.pkl'
 OUT_NAME = f'L_{LAG}_Q_{QUANT}_EM_{EMBED}'
 
 out_dict = {}
@@ -51,15 +51,15 @@ if not MATCHES_ONLY:
 #matches = [matches[0]]
 for m in tqdm(matches):
     try:
-        val_data = MLP_dataset(path=DATA_PATH, train=False, lag=LAG, get_quant=QUANT, normalize=NORMALIZE,
-                               embedders=embedders, matches=m)
+        val_data = MLP_dataset_emb(path=DATA_PATH, train=False, lag=LAG, get_quant=QUANT, normalize=NORMALIZE,
+                                   embedders=embedders, matches=m)
 
         b_size = val_data.y_lag.shape[0] - 1
 
         val_dataloader = DataLoader(val_data, batch_size=b_size, shuffle=True, num_workers=15)
 
-        input, flag = next(iter(val_dataloader))
-        model_output = model(input)
+        emb_2, emb_3, X, flag = next(iter(val_dataloader))
+        model_output = model(emb_2, emb_3, X)
         rmse_test = loss_fn(model_output, flag).detach().cpu().numpy().item()
 
         out_dict[f'{m[0]}_{m[1]}'] = {'rmse_train': np.nan, 'rmse_test': rmse_test}
