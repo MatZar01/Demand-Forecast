7e3cb85e13853ebfed094f872fd6ad9f81f78613
MatZar02 <mateusz.zarski@newsecuritysystems.pl>
2024-10-13 01:24:21 +0200

    model clustering 10 15 20 - results

--- baseline/results/name_clustering/results.py
+++ baseline/results/name_clustering/results.py
@@ -6,18 +6,42 @@ CLUSTERING 25/50/100
 """
 
 data_cluster = pickle.load(open('/home/mateusz/Desktop/Demand-Forecast/baseline/results/name_clustering/model_out_clustering.pkl', 'rb'))
+data_nums = pickle.load(open('/home/mateusz/Desktop/Demand-Forecast/baseline/results/name_clustering/emb_assignments.pkl', 'rb'))
 
 # 25 models
 rmses = data_cluster[25]
-mean_rmse_25 = np.mean(rmses)
+nums = data_nums[25]
+rmse_25_w = 0
+rmse_25_nums = 0
+for i in range(len(rmses)):
+      rmse_25_w += rmses[i] * len(nums[i])
+      rmse_25_nums += len(nums[i])
+
+mean_rmse_25 = rmse_25_w/rmse_25_nums
+
 # 50 models
 rmses = data_cluster[50]
-mean_rmse_50 = np.mean(rmses)
+nums = data_nums[50]
+rmse_50_w = 0
+rmse_50_nums = 0
+for i in range(len(rmses)):
+      rmse_50_w += rmses[i] * len(nums[i])
+      rmse_50_nums += len(nums[i])
+
+mean_rmse_50 = rmse_50_w/rmse_50_nums
+
 # 100 models
 rmses = data_cluster[100]
-mean_rmse_100 = np.mean(rmses)
+nums = data_nums[100]
+rmse_100_w = 0
+rmse_100_nums = 0
+for i in range(len(rmses)):
+      rmse_100_w += rmses[i] * len(nums[i])
+      rmse_100_nums += len(nums[i])
 
-print(f'CLUSTERING RESULTS\n'
+mean_rmse_100 = rmse_100_w/rmse_100_nums
+
+print(f'CLUSTERING RESULTS 25-50-100\n'
       f'25 models: {mean_rmse_25}\n'
       f'50 models: {mean_rmse_50}\n'
       f'100 models: {mean_rmse_100}\n\n')
@@ -31,21 +55,45 @@ CLUSTERING 10/15/20
 """
 
 data_cluster = pickle.load(open('/home/mateusz/Desktop/Demand-Forecast/baseline/results/name_clustering/model_out_clustering_10_15_20.pkl', 'rb'))
+data_nums = pickle.load(open('/home/mateusz/Desktop/Demand-Forecast/baseline/results/name_clustering/emb_assignments_10_15_20.pkl', 'rb'))
+
+# 10 models
+rmses = data_cluster[10]
+nums = data_nums[10]
+rmse_10_w = 0
+rmse_10_nums = 0
+for i in range(len(rmses)):
+      rmse_10_w += rmses[i] * len(nums[i])
+      rmse_10_nums += len(nums[i])
+
+mean_rmse_10 = rmse_10_w/rmse_10_nums
+
+# 15 models
+rmses = data_cluster[15]
+nums = data_nums[15]
+rmse_15_w = 0
+rmse_15_nums = 0
+for i in range(len(rmses)):
+      rmse_15_w += rmses[i] * len(nums[i])
+      rmse_15_nums += len(nums[i])
+
+mean_rmse_15 = rmse_15_w/rmse_15_nums
 
 # 25 models
-rmses = data_cluster[10]
-mean_rmse_25 = np.mean(rmses)
-# 50 models
-rmses = data_cluster[15]
-mean_rmse_50 = np.mean(rmses)
-# 100 models
 rmses = data_cluster[20]
-mean_rmse_100 = np.mean(rmses)
+nums = data_nums[20]
+rmse_20_w = 0
+rmse_20_nums = 0
+for i in range(len(rmses)):
+      rmse_20_w += rmses[i] * len(nums[i])
+      rmse_20_nums += len(nums[i])
 
-print(f'CLUSTERING RESULTS\n'
-      f'10 models: {mean_rmse_25}\n'
-      f'15 models: {mean_rmse_50}\n'
-      f'20 models: {mean_rmse_100}\n\n')
+mean_rmse_20 = rmse_20_w/rmse_20_nums
+
+print(f'CLUSTERING RESULTS 10-15-20\n'
+      f'10 models: {mean_rmse_10}\n'
+      f'15 models: {mean_rmse_15}\n'
+      f'20 models: {mean_rmse_20}\n\n')
 #%%
 import pickle
 import numpy as np
