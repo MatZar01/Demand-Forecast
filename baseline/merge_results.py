19d311dbc674c2768146bde830ad61f73f50882e
nuwan.gunasekara <ng198002@gmail.com>
2024-10-30 15:55:59 +0100

    add demand forcasting results

---
+++ baseline/merge_results.py
@@ -0,0 +1,21 @@
+
+
+import os
+import pickle
+
+args_dir = '/DS/demand-forecasting_LAG_0'
+with open(os.path.join(args_dir, 'results_half.pkl'), 'rb') as f:
+    results1 = pickle.load(f)
+
+args_dir = '/DS/demand-forecasting_LAG_0_1'
+with open(os.path.join(args_dir, 'results.pkl'), 'rb') as f:
+    results2 = pickle.load(f)
+
+
+for l_name, l in results2['incremental_learners'].items():
+    results1['incremental_learners'][l_name] = l
+
+
+args_dir = '/DS/demand-forecasting_LAG_0'
+with open(os.path.join(args_dir, 'results.pkl'), 'wb') as f:
+    pickle.dump(results1, f)
